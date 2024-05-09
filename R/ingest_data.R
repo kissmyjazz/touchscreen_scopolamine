2141a22701cc51b9052997f4968d59bef3a7d6a6
Denis Matrov <jazzhole@gmail.com>
2024-05-09 17:42:35 -0400

    I finished pilot ingestion file

--- R/ingest_data.R
+++ R/ingest_data.R
@@ -1,6 +1,7 @@
 library(here)
 library(tidyverse)
 library(readxl)  # for reading excel files
+library(writexl)
 
 
 # regex patterns
@@ -27,6 +28,16 @@ df <- df_ |>
   dplyr::mutate(mouse_x_last = stringi::stri_match_last_regex(mouse.x, ptn_last_number)[, 1],
                 .after = mouse.x) |>
   dplyr::mutate(mouse_y_last = stringi::stri_match_last_regex(mouse.y, ptn_last_number)[, 1],
-                .after = mouse.y)
+                .after = mouse.y) |>
+  dplyr::mutate(mouse2_x_last = stringi::stri_match_last_regex(mouse_2.x, ptn_last_number)[, 1],
+                .after = mouse_2.x) |>
+  dplyr::mutate(mouse2_y_last = stringi::stri_match_last_regex(mouse_2.y, ptn_last_number)[, 1],
+                .after = mouse_2.y) |>
+  dplyr::mutate(mouse_time_last = stringi::stri_match_last_regex(mouse.time, ptn_last_number)[, 1],
+                .after = mouse.time) |>
+  dplyr::mutate(mouse2_time_last = stringi::stri_match_last_regex(mouse_2.time, ptn_last_number)[, 1],
+                .after = mouse_2.time) |>
+  dplyr::mutate(mouse_num_touch = str_count(mouse.x, ptn_last_number), .after = mouse_x_last) |>
+  dplyr::mutate(mouse2_num_touch = str_count(mouse_2.x, ptn_last_number), .after = mouse2_x_last)
 
-
+write_xlsx(df, here("data", "combined_data.xlsx"))
